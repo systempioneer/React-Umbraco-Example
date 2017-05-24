774c67733fd73859227ec2c2c07b93967b7e5935
Gary Cheetham <yourstruly@glcheetham.name>
2017-05-24 12:23:15 +0100

    Experimentally rendering content from server

--- UmbracoReactStarterKit/app/src/App.js
+++ UmbracoReactStarterKit/app/src/App.js
@@ -3,16 +3,33 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { currentContent: '' }
+  }
+
+  componentDidMount() {
+    fetch('/umbraco/surface/rendercontent/byid/1064', { credentials: 'same-origin' })
+      .then((response) => {
+        if (response.ok) {
+          return response.text()
+        }
+        return Promise.reject(response)
+      })
+      .then(result => { this.setState({ currentContent: result }) })
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
+      <div className="App" dangerouslySetInnerHTML={{ __html: this.state.currentContent }}>
+        {/*<div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>*/}
       </div>
     );
   }
--- UmbracoReactStarterKit/app/src/index.js
+++ UmbracoReactStarterKit/app/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
