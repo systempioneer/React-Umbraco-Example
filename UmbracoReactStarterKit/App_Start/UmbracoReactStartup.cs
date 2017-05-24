0e20e7eafc1ca7d95d27cc3c6f4b6c339feded86
Gary Cheetham <yourstruly@glcheetham.name>
2017-05-24 11:08:45 +0100

    Added surface controller to render content

---
+++ UmbracoReactStarterKit/App_Start/UmbracoReactStartup.cs
@@ -0,0 +1,55 @@
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autofac.Integration.WebApi;
+using Examine;
+using Umbraco.Core;
+using Umbraco.Web;
+
+namespace UmbracoReactStarterKit
+{
+    public class UmbracoReactStartup : IApplicationEventHandler
+    {
+        public void OnApplicationInitialized(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+        }
+
+        public void OnApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            // This project uses dependency injection
+            // Read more about umbraco and dependency injection here:
+            // https://glcheetham.name/2016/05/27/implementing-ioc-in-umbraco-unit-testing-like-a-boss/
+            // https://glcheetham.name/2017/01/29/mocking-umbracohelper-using-dependency-injection-the-right-way/
+
+
+            var umbracoContext = umbracoApplication.Context.GetUmbracoContext();
+
+            var builder = new ContainerBuilder();
+
+            var umbracoHelper = new Umbraco.Web.UmbracoHelper(umbracoContext);
+
+            // Register our controllers from this assembly with Autofac
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            // Register controllers from the Umbraco assemblies with Autofac
+            builder.RegisterControllers(typeof(UmbracoApplication).Assembly);
+            builder.RegisterApiControllers(typeof(UmbracoApplication).Assembly);
+            // Register the types we need to resolve with Autofac
+            builder.RegisterInstance(ExamineManager.Instance).As<ExamineManager>();
+            builder.RegisterInstance(umbracoHelper.ContentQuery).As<ITypedPublishedContentQuery>();
+
+            // Set up MVC to use Autofac as a dependency resolver
+            var container = builder.Build();
+            System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
+            // And WebAPI
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+
+        }
+    }
+}
---
+++ UmbracoReactStarterKit/Controllers/RenderContentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Web.Mvc;
+using Examine;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbracoReactStarterKit.Controllers
+{
+    // All hail shazwazza https://shazwazza.com/post/Custom-MVC-routing-in-Umbraco
+    public class RenderContentController : SurfaceController
+    {
+        private readonly ExamineManager _examineManager;
+
+        public RenderContentController(ExamineManager examineManager)
+        {
+            _examineManager = examineManager;
+        }
+
+        public ActionResult ById(string id)
+        {
+            var criteria = _examineManager.DefaultSearchProvider
+                .CreateSearchCriteria("content");
+            var filter = criteria.Id(Convert.ToInt32(id));
+            var result = Umbraco.TypedSearch(filter.Compile()).ToArray();
+            if (!result.Any())
+            {
+                return HttpNotFound();
+            }
+            var renderModel = CreateRenderModel(result.First());
+
+            return View(renderModel.Content.GetTemplateAlias(), renderModel);
+        }
+
+        private RenderModel CreateRenderModel(IPublishedContent content)
+        {
+            var model = new RenderModel(content, CultureInfo.CurrentUICulture);
+
+            //add an umbraco data token so the umbraco view engine executes
+            RouteData.DataTokens["umbraco"] = model;
+
+            return model;
+        }
+    }
+}
